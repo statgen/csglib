b2c68e38365f98c37c13150bc7102feb9ed1e1af
Daniel Taliun <dtaliun@umich.edu>
2017-05-29 18:45:03 -0400

    tool for getting disjunct loci from GWAS hits

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     'cython',
     'numpy',
   ],
-  scripts = ['csg/gwas/GCcorrect', 'csg/gwas/IndependentHits', 'csg/gwas/InflationFromFile', 'csg/pedigree/trios/TrioPuller'],
+  scripts = ['csg/gwas/SignificantLoci', 'csg/gwas/GCcorrect', 'csg/gwas/IndependentHits', 'csg/gwas/InflationFromFile', 'csg/pedigree/trios/TrioPuller'],
 )
 
 
