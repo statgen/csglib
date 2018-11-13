56b27820ffaace189f921e54cf4afb01f23a1b71
Ryan Welch <welchr@umich.edu>
2018-11-13 16:22:57 -0500

    Fix cython in setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
+from setuptools import setup
+from setuptools.extension import Extension
+from Cython.Build import cythonize
+import numpy
 
-from distutils.core import setup
-from distutils.extension import Extension
+extensions = cythonize("csg/**/*.pyx")
+for e in extensions:
+  e.include_dirs.append(numpy.get_include())
 
-def build_ext(*args, **kwargs):
-  from Cython.Distutils import build_ext
-  return build_ext(*args, **kwargs)
+with open("requirements.txt","rt") as f:
+  requirements = f.read().split()
 
 setup(
   name = "csg",
-  version = "0.2",
+  version = "0.2.1",
   author_email = "",
   author = "Center for Statistical Genetics",
   packages = [
@@ -22,42 +26,27 @@ setup(
     "csg/pedigree",
     "csg/gwas"
   ],
-  cmdclass = {"build_ext" : build_ext},
-  ext_modules = [
-    Extension(
-      "csg.statistics.invnorm",
-      sources = ["csg/statistics/invnorm.pyx"],
-    ),
-    Extension(
-      "csg.genetics.cyalleles",
-      sources = ["csg/genetics/cyalleles.pyx"],
-    )
-  ],
   classifiers = [
-    'Development Status :: 4 - Beta',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.5',
-    'Topic :: Scientific/Engineering :: Bio-Informatics'
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Science/Research",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.5",
+    "Topic :: Scientific/Engineering :: Bio-Informatics"
   ],
-  setup_requires = [
-    'setuptools>=18.0',
-    'cython',
-    'numpy',
-  ],
+  setup_requires = ["Cython"],
+  install_requires = requirements,
+  ext_modules = extensions,
+  zip_safe = False,
   scripts = [
-      'csg/gwas/Harmonize/Harmonize',
-      'csg/gwas/TransformEffect',
-      'csg/gwas/Filter',
-      'csg/gwas/AnnotateRs',
-      'csg/gwas/SignificantLoci',
-      'csg/gwas/GCcorrect',
-      'csg/gwas/IndependentHits',
-      'csg/gwas/InflationFromFile',
-      'csg/pedigree/trios/TrioPuller'
+    "csg/gwas/Harmonize/Harmonize",
+    "csg/gwas/TransformEffect",
+    "csg/gwas/Filter",
+    "csg/gwas/AnnotateRs",
+    "csg/gwas/SignificantLoci",
+    "csg/gwas/GCcorrect",
+    "csg/gwas/IndependentHits",
+    "csg/gwas/InflationFromFile",
+    "csg/pedigree/trios/TrioPuller"
   ],
 )
-
-
-# -*- python-indent-offset: 2 -*-
